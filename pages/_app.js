8a74c2db55029c8a4710f2dfddb2b2c3e862e326
UrielMarles <marlesurielnicolas@gmail.com>
2023-12-14 11:14:15 -0300

    para asegurar

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,8 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
+import Head from 'next/head';
 
 export default function App({Component,papeProps}){
-    return(<Component {...papeProps}/>)
+    return(<><Head><link rel="icon" href="/favicon.ico" /></Head><Component {...papeProps}/></>)
 }
